2eed5ff7c223c744b1e4bb83ed54c869ca9b1c49
Neto Moraes <jomoraesneto@gmail.com>
2025-03-28 00:17:51 -0300

    feat: add quantum moon planet card

--- items/planets/quantum_moon.lua
+++ items/planets/quantum_moon.lua
@@ -7,5 +7,26 @@ LoadConsumable {
         text = {
             "Upgrade {C:attention}a random hand{} by {C:attention}3 levels{C:attention}"
         }
-    }
+    },
+    cost = 3,
+    can_use = function(self, card)
+        return true
+    end,
+
+    use = function(self, card, area, copier)
+        local random_hand_name = pseudorandom_element(G.handlist, pseudoseed("randomhand"))
+        local random_hand = G.GAME.hands[random_hand_name]
+
+        local used_consumable = copier or card
+        update_hand_text(
+            { sound = 'button', volume = 0.7, pitch = 0.8, delay = 0.3 },
+            { handname = localize(random_hand_name, "poker_hands"), chips = random_hand.chips, mult = random_hand.mult, level =
+            random_hand.level })
+
+        level_up_hand(used_consumable, random_hand_name, false, 3)
+
+        update_hand_text(
+            { sound = 'button', volume = 0.7, pitch = 1.1, delay = 0 },
+            { mult = 0, chips = 0, handname = '', level = '' })
+    end,
 }
